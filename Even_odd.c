e231602bd983ec14b9eb1d97bc76953fed894ba4
shivamyadav870085-maker <shivamyadav870085@gmail.com>
2025-10-01 12:53:16 +0530

    Create Even_odd.c

---
+++ Even_odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+2 int main (){
+3 int x;
+4 printf("enter a number:");
+5 scanf("%d",&x);
+6 if(x%2==0){
+7 printf ("even number");}
+8 if(x%2!=0){
+9 printf("odd number");}
+10 return 0;
+11}
