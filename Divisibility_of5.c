db1e9f0bbb1b179382e79fd4cde1558b970257c4
shivamyadav870085-maker <shivamyadav870085@gmail.com>
2025-10-01 13:23:12 +0530

    Create Divisibility_of5.c

---
+++ Divisibility_of5.c
@@ -0,0 +1,23 @@
+1 #include <stdio.h>
+
+2 int main (){
+
+3 int x;
+
+4 printf("enter a number:");
+
+5 scanf("%d", &x);
+
+6 if(x%5==0){
+
+7 printf("divisible by 5");}
+
+8 else{
+
+9 printf("not divosible by 5");}
+
+10
+
+11 return 0;
+
+12 }
